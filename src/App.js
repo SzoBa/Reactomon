ecdfce8d3f4c76c4e47de558412bbd2f49f3b112
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-08 23:21:25 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="container"></div>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Navbar />
+            {/* <Route path="/pokemons" component={PokemonList} />
+            <Route path="/types" component={TypeList} />
+            <Route path="/pokemon/:id" component={PokemonDetail} /> */}
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
