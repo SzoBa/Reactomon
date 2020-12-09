7c87c4162c407f9c65397f429f7f1aaf79f99621
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-09 13:47:08 +0100

    Add list items html elements

---
+++ src/components/pages/PokemonListItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "../../App.css";
+
+class PokemonListItem extends Component {
+  render() {
+    const { name, url } = this.props.pokemon;
+    const id = url.match("/[0-9]+/").toString().replaceAll("/", "");
+
+    return (
+      <p className="listItem">
+        Name: {name} - <Link to={{ pathname: `/pokemon/${id}` }}>Details</Link>{" "}
+        - Original url: {url}
+      </p>
+    );
+  }
+}
+
+PokemonListItem.propTypes = {
+  pokemon: PropTypes.array.isRequired,
+};
+
+export default PokemonListItem;
