0cdf16af5d045325be3881612cc1a9adfab448c1
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-09 13:46:45 +0100

    Add list class

--- src/components/pages/PokemonList.js
+++ src/components/pages/PokemonList.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
+import PokemonListItem from "./PokemonListItem";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 class PokemonList extends Component {
   state = {
     pokemons: [],
   };
 
-  getPokemons = () => {
+  componentDidMount() {
     axios
       .get("https://pokeapi.co/api/v2/pokemon")
-      .then((res) => this.setState({ pokemons: res.data }));
-  };
+      .then((res) => this.setState({ pokemons: res.data.results }));
+  }
 
   render() {
-    return <div></div>;
+    return this.state.pokemons.map((pokemon) => (
+      <div>
+        <PokemonListItem pokemon={pokemon} />
+      </div>
+    ));
   }
 }
+
+PokemonList.propTypes = {
+  pokemons: PropTypes.array.isRequired,
+};
 
 export default PokemonList;
