1cf4a04b9613802acddfa0520f54c8485705df9e
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-10 22:55:49 +0100

    Change simpleItem

--- src/components/pages/SimpleItem.js
+++ src/components/pages/SimpleItem.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
-
-class SimpleItem extends Component {
-  render() {
-    return (
-      <div className="simpleData">
-        <p>{this.props.attribute + ": " + this.props.simpleData.toString()}</p>
-      </div>
-    );
-  }
-}
+const SimpleItem = (props) => {
+  return (
+    <div className="simpleData">
+      <p>{props.attribute + ": " + props.simpleData.toString()}</p>
+    </div>
+  );
+};
 
 export default SimpleItem;
