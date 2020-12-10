a0c3eb7189e78e1e858f6398ae08029e129b69b6
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-10 22:52:13 +0100

    Change main page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,15 +1,12 @@
-import React, { Component } from "react";
 import "../../App.css";
 
-class MainPage extends Component {
-  render() {
-    return (
-      <div className="mainPage">
-        <h1>This is the Main Page</h1>
-        <p>Choose from the links above to navigate to the desired page</p>
-      </div>
-    );
-  }
-}
+const MainPage = (props) => {
+  return (
+    <div className="mainPage">
+      <h1>This is the Main Page</h1>
+      <p>Choose from the links above to navigate to the desired page</p>
+    </div>
+  );
+};
 
 export default MainPage;
