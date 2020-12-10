466c9918557f4a81b345b0b695829d2d9795bb35
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-10 22:51:56 +0100

    Change navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <div className="navBar">
-        <h1>Reactomon</h1>
-        <div className="links">
-          <Link to="/pokemons">Pokemons</Link>
-          <Link to="/types">Types</Link>
-        </div>
+const Navbar = (props) => {
+  return (
+    <div className="navBar">
+      <h1>Reactomon</h1>
+      <div className="links">
+        <Link to="/pokemons">Pokemons</Link>
+        <Link to="/types">Types</Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Navbar;
