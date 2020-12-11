32ebfc3150cedd6035f3f0aaa55a49a89906a6cf
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 19:48:46 +0100

    Add type list cards

--- src/components/pages/TypeList.js
+++ src/components/pages/TypeList.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { useFetch } from "../../hooks/useFetch";
 import TypeListItem from "./TypeListItem";
+import styled from "styled-components";
+
+const TypeListContainer = styled.div`
+  display: inline-flex;
+  margin: 1% 2%;
+`;
 
 const TypeList = (props) => {
   const [isLoading, types] = useFetch("https://pokeapi.co/api/v2/type");
 
   if (!isLoading) {
     return types.results.map((type, index) => (
-      <div key={index}>
+      <TypeListContainer key={index}>
         <TypeListItem type={type} index={index} />
-      </div>
+      </TypeListContainer>
     ));
   } else {
     return (
