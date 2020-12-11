4a7217b9d34c309e721708ccfefc528c095fc875
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 02:37:51 +0100

    Change class to function

--- src/components/pages/PokemonListItem.js
+++ src/components/pages/PokemonListItem.js
@@ -1,24 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import "../../App.css";
 
-class PokemonListItem extends Component {
-  render() {
-    const { name, url } = this.props.pokemon;
-    const id = url.match("/[0-9]+/").toString().replaceAll("/", "");
+const PokemonListItem = (props) => {
+  const { name, url } = props.pokemon;
+  const id = url.match("/[0-9]+/").toString().replaceAll("/", "");
 
-    return (
-      <p className="listItem">
-        Name: {name} - <Link to={{ pathname: `/pokemon/${id}` }}>Details</Link>{" "}
-        - Original url: {url}
-      </p>
-    );
-  }
-}
-
-PokemonListItem.propTypes = {
-  pokemon: PropTypes.object.isRequired,
+  return (
+    <p className="listItem">
+      Name: {name} - <Link to={{ pathname: `/pokemon/${id}` }}>Details</Link> -
+      Original url: {url}
+    </p>
+  );
 };
 
 export default PokemonListItem;
