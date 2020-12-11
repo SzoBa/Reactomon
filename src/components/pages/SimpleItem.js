470f06109f446bfc798e21b52f1a92b7dd314d29
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 13:53:05 +0100

    Refactor style

--- src/components/pages/SimpleItem.js
+++ src/components/pages/SimpleItem.js
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+const SimpleAttributeText = styled.p`
+  font-size: 1em;
+  margin: 1%;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: center;
+`;
+
 const SimpleItem = (props) => {
   return (
     <div className="simpleData">
-      <p>{props.attribute + ": " + props.simpleData.toString()}</p>
+      <SimpleAttributeText>
+        {props.attribute + ": " + props.simpleData.toString()}
+      </SimpleAttributeText>
     </div>
   );
 };
