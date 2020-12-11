f416a58d6afad158890cd211f07e1239aa930723
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 12:47:15 +0100

    Add main page styling

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,10 +1,29 @@
+import styled from "styled-components";
 import "../../App.css";
+
+const MainPageH1 = styled.h1`
+  margin-top: 3%;
+  font-size: 4em;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: center;
+`;
+
+const MainPageText = styled.p`
+  font-size: 2em;
+  margin-top: 3%;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: center;
+`;
 
 const MainPage = (props) => {
   return (
-    <div className="mainPage">
-      <h1>This is the Main Page</h1>
-      <p>Choose from the links above to navigate to the desired page</p>
+    <div>
+      <MainPageH1>This is the Main Page</MainPageH1>
+      <MainPageText>
+        Choose from the links above to navigate to the desired page
+      </MainPageText>
     </div>
   );
 };
