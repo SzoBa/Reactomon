64e9dec1ee7482afbea331d15810563d7b1ce6dc
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 12:17:26 +0100

    Add fetch version2

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,27 +1,43 @@
 import { useState, useEffect } from "react";
-import Axios from "axios";
+// import Axios from "axios";
+import axios from "axios";
 
 export const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [fetchedData, setFetchedData] = useState([]);
 
+  // useEffect(() => {
+  //   let mounted = true;
+  //   const loadData = async () => {
+  //     const response = await Axios.get(url);
+  //     if (mounted) {
+  //       setFetchedData(response.data);
+  //       if (fetchedData !== []) {
+  //         setIsLoading(false);
+  //       }
+  //     }
+  //   };
+  //   loadData();
+  //   return () => {
+  //     mounted = false;
+  //   };
+  // }, [url, fetchedData]);
+
   useEffect(() => {
     let mounted = true;
-    const loadData = async () => {
-      const response = await Axios.get(url);
-      if (mounted) {
-        setFetchedData(response.data);
+    axios
+      .get(url)
+      .then((response) => {
+        if (mounted) {
+          setFetchedData(response.data);
+        }
+      })
+      .then(() => {
         if (fetchedData !== []) {
           setIsLoading(false);
         }
-      }
-    };
-    loadData();
-
-    return () => {
-      mounted = false;
-    };
+      });
+    return () => (mounted = false);
   }, [url, fetchedData]);
-
   return [isLoading, fetchedData];
 };
