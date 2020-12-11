453ff22a8ed456ed51bf08a2499085901a7d6cd9
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 02:37:12 +0100

    Change class to function

--- src/components/pages/PokemonDetail.js
+++ src/components/pages/PokemonDetail.js
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
-import DetailItem from "./DetailItem";
+import React, { useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
 import SimpleItem from "./SimpleItem";
 
-class PokemonDetail extends Component {
-  state = {
-    id: [],
-    details: [],
-  };
-
-  componentDidMount() {
-    this.setState({ id: this.props.match.params.id }, () => {
-      axios
-        .get(`https://pokeapi.co/api/v2/pokemon/${this.state.id}`)
-        .then((res) => this.setState({ details: res.data }));
-    });
+const PokemonDetail = (props) => {
+  const [id, setId] = useState(null);
+  if (id === null) {
+    setId(props.match.params.id);
   }
 
-  render() {
-    return Object.entries(this.state.details).map(([key, value], index) => (
+  const [
+    isLoading,
+    details,
+  ] = useFetch(`https://pokeapi.co/api/v2/pokemon/${id}`, [id]);
+
+  if (!isLoading) {
+    return Object.entries(details).map(([key, value], index) => (
       <div className="detailItem" key={key}>
         <React.Fragment>
           {typeof value === "number" ||
@@ -33,17 +28,13 @@ class PokemonDetail extends Component {
         </React.Fragment>
       </div>
     ));
+  } else {
+    return (
+      <div className="detailItem">
+        <p>Problem occurred during loading!</p>
+      </div>
+    );
   }
-}
-
-PokemonDetail.propTypes = {
-  attribute: PropTypes.string,
-  simpleData: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
-  ]),
-  detailData: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
 
 export default PokemonDetail;
