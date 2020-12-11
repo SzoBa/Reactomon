5b93bae2c25d16963f4fc2363e01d378bb9de26e
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-11 12:47:31 +0100

    Add pokemonlist styling

--- src/components/pages/PokemonListItem.js
+++ src/components/pages/PokemonListItem.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import "../../App.css";
+
+const ListItemText = styled.p`
+  font-size: 1em;
+  margin: 1%;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: center;
+`;
+
+const ListItemLink = styled.a`
+  margin: 1%;
+  font-size: 1em;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: center;
+`;
 
 const PokemonListItem = (props) => {
   const { name, url } = props.pokemon;
   const id = url.match("/[0-9]+/").toString().replaceAll("/", "");
 
   return (
-    <p className="listItem">
-      Name: {name} - <Link to={{ pathname: `/pokemon/${id}` }}>Details</Link> -
+    <ListItemText>
+      Name: {name} -{" "}
+      <ListItemLink to={{ pathname: `/pokemon/${id}` }}>Details</ListItemLink> -
       Original url: {url}
-    </p>
+    </ListItemText>
   );
 };
 
