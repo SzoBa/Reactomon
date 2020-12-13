45e61050f3a3dfc20ed3dc1407a85b3a17eb2bc4
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-13 16:04:37 +0100

    Add context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import ThemeContext from "./contexts/ThemeContext";
+import { ColorProvider } from "./contexts/ColorContext";
+import { CatchPokemonProvider } from "./contexts/CatchPokemonContext";
 import Navbar from "./components/layout/Navbar";
+import MainBody from "./elements/MainBody";
 import MainPage from "./components/pages/MainPage";
 import PokemonList from "./components/pages/PokemonList";
 import TypeList from "./components/pages/TypeList";
 import PokemonDetail from "./components/pages/PokemonDetail";
-import { ThemeProvider, css } from "styled-components";
-import MainBody from "./elements/MainBody";
-import { ColorProvider } from "./ColorContext";
 import CatchedList from "./components/pages/CatchedList";
-import { CatchPokemonProvider } from "./CatchPokemonContext";
-
-const theme = {
-  primary: "junk",
-  secondary: "blade",
-};
 
 const App = (props) => {
+  const theme = useState("junk");
+
   return (
-    <ThemeProvider theme={theme}>
-      <CatchPokemonProvider>
-        <ColorProvider>
+    <ThemeContext.Provider value={theme}>
+      <ColorProvider>
+        <CatchPokemonProvider>
           <Router>
             <MainBody>
               <Navbar />
@@ -32,9 +29,9 @@ const App = (props) => {
               <Route path="/catched" component={CatchedList} />
             </MainBody>
           </Router>
-        </ColorProvider>
-      </CatchPokemonProvider>
-    </ThemeProvider>
+        </CatchPokemonProvider>
+      </ColorProvider>
+    </ThemeContext.Provider>
   );
 };
 
