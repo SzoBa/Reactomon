748ec08c0a64b97f5cd3f9fd2084465db89aba26
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-14 22:18:00 +0100

    Refactor fetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,27 +1,9 @@
 import { useState, useEffect } from "react";
-// import Axios from "axios";
 import axios from "axios";
 
 export const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [fetchedData, setFetchedData] = useState([]);
-
-  // useEffect(() => {
-  //   let mounted = true;
-  //   const loadData = async () => {
-  //     const response = await Axios.get(url);
-  //     if (mounted) {
-  //       setFetchedData(response.data);
-  //       if (fetchedData !== []) {
-  //         setIsLoading(false);
-  //       }
-  //     }
-  //   };
-  //   loadData();
-  //   return () => {
-  //     mounted = false;
-  //   };
-  // }, [url, fetchedData]);
 
   useEffect(() => {
     axios
